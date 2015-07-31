522dd699a27b5e226253146f91221ccba7cf8398
Nat Friedman <nat@xamarin.com>
2015-07-31 15:08:13 -0600

    Adding OAuth constants for Google

--- iOS/Renderers/LoginPageRenderer.cs
+++ iOS/Renderers/LoginPageRenderer.cs
@@ -12,12 +12,15 @@ namespace MailStats.iOS
 		{
 			base.ViewDidAppear (animated);
 
+
 			var auth = new OAuth2Authenticator (
-				// FIXME What do I put in here?
-				clientId: "", // your OAuth2 client id
-				scope: "", // the scopes for the particular API you're accessing, delimited by "+" symbols
-				authorizeUrl: new Uri (""), // the auth URL for the service
-				redirectUrl: new Uri ("")); // the redirect URL for the service
+				"900358175438-rrk0rqp76dd5l24jsevmunjm1277an50.apps.googleusercontent.com", // your OAuth2 client id
+				"W6CsgvukJ3YxWrhSs98B7Q22", // client secret
+				"gmail.readonly", // the scopes for the particular API you're accessing, delimited by "+" symbols
+				new Uri ("https://accounts.google.com/o/oauth2/auth"), // the auth URL for the service
+				new Uri ("urn:ietf:wg:oauth:2.0:oob\n"), // redirect URL
+				new Uri ("https://accounts.google.com/o/oauth2/token") // access token URL
+			);
 
 			auth.Completed += (sender, eventArgs) => {
 				// We presented the UI, so it's up to us to dimiss it on iOS.
