66c0fc898fe5d037b57617dedb8ca70825b68958
Nat Friedman <nat@xamarin.com>
2015-08-01 17:29:07 -0600

    Added insights identify

--- MailStats/LoginPage.cs
+++ MailStats/LoginPage.cs
@@ -4,6 +4,9 @@ using SimpleAuth;
 using SimpleAuth.Providers;
 using System.Threading.Tasks;
 
+using Xamarin;
+using System.Collections.Generic;
+
 namespace MailStats
 {
 	public class LoginPage : ContentPage
@@ -23,7 +26,16 @@ namespace MailStats
 				App.GoogleUser = new GoogleUser();
 				App.GoogleUser.Email = profile.Email;
 				App.GoogleUser.AccessToken = api.CurrentOAuthAccount.Token;
-				
+
+				var traits = new Dictionary<string, string> {
+					{Insights.Traits.Email, profile.Email},
+					{Insights.Traits.Gender, profile.Gender},
+					{Insights.Traits.FirstName, profile.GivenName},
+					{Insights.Traits.LastName, profile.FamilyName},
+					{Insights.Traits.Avatar, profile.Picture}
+				};
+				Insights.Identify(profile.Id, traits);
+
 				App.SuccessfulLoginAction.Invoke ();
 			}
 			catch (TaskCanceledException ex)
